0f1d77aa34902db0dc8f50246d89c7b7077ed38c
Loic Reyreaud <reyrea_l@epita.fr>
2017-07-03 04:10:42 +0200

    Updated rendering of plotting

--- dataset/converter.py
+++ dataset/converter.py
@@ -2,7 +2,6 @@
 
 import json
 import math
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,22 +9,18 @@ data = json.load(open('./us_cities.json'))
 out = []
 earth_radius = 6371
 for item in data:
-    lat = item["latitude"] * 3.14 / 180
-    lon = item["longitude"] * 3.12 / 180
+    lat = item["latitude"] * math.pi / 180
+    lon = item["longitude"] * math.pi / 180
     x = earth_radius * math.cos(lon) * math.sin(lat)
     y = earth_radius * math.sin(lat) * math.sin(lon)
-    z = earth_radius * math.cos(lat)
-    out.append(json.dumps({"name": item["city"], "x": x, "y": y, "z": z}))
+    out.append(json.dumps({"name": item["city"], "x": x, "y": y}))
 json.dump(out, open('./out.json', 'w'))
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+plt.gca().invert_yaxis()
 
 for item in out:
     point = json.loads(item)
-    ax.scatter(point["x"], point["y"], zdir='z')
+    plt.scatter(point["x"], point["y"])
 
 plt.show()
