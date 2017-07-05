5254995b6039fccca206691a01ac8ee684fb236e
Mickael IDE <mickael.ide@epita.fr>
2017-07-06 01:29:49 +0200

    Added converter json -> csv

--- dataset/converter.py
+++ dataset/converter.py
@@ -4,16 +4,22 @@ import json
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
-data = json.load(open('./us_cities.json'))
+data = json.load(open(sys.argv[1]))
 out = []
 earth_radius = 6371
 for item in data:
-    lat = item["latitude"] * math.pi / 180
-    lon = item["longitude"] * math.pi / 180
-    x = earth_radius * math.cos(lon) * math.sin(lat)
-    y = earth_radius * math.sin(lat) * math.sin(lon)
-    out.append(json.dumps({"name": item["city"], "x": x, "y": y}))
+    try:
+        lat = float(item["latitude"]) * math.pi / 180
+        lon = float(item["longitude"]) * math.pi / 180
+        x = earth_radius * math.cos(lon) * math.sin(lat)
+        y = earth_radius * math.sin(lat) * math.sin(lon)
+        out.append(json.dumps({"name": item["city"], "x": x, "y": y}))
+    except:
+        continue
+
+
 json.dump(out, open('./out.json', 'w'))
 
 fig = plt.figure()
---
+++ dataset/json_to_dot.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import json
+import math
+import sys
+
+data = json.load(open(sys.argv[1]))
+out = []
+earth_radius = 6371
+file = open("out.dot", "w")
+
+for item in data:
+    try:
+        lat = float(item["latitude"]) * math.pi / 180
+        lon = float(item["longitude"]) * math.pi / 180
+    except:
+        continue
+    x = int(earth_radius * math.cos(lon) * math.sin(lat))
+    y = int(earth_radius * math.sin(lat) * math.sin(lon))
+    file.write(item["city"] + "," + str(x) + "," + str(y) + "\n")
+
+file.close()
