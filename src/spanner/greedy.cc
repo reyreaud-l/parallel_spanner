b3660ae259f460962d365c4bf67bfd8e5d7f2a41
Loic Reyreaud <reyrea_l@epita.fr>
2017-07-08 14:16:51 +0200

    Reverted change regarding attempt to parallelize dijkstra

--- src/spanner/greedy.cc
+++ src/spanner/greedy.cc
@@ -1,4 +1,3 @@
-#include "tbb/tbb.h"
 #include "spanner/spanner.hh"
 
 
@@ -71,22 +70,16 @@ dijkstra(Graph<>& graph, Graph<>::vertex_descriptor src,
       if (dest == vertice)
         break;
 
-      auto it = boost::adjacent_vertices(vertice, graph);
-      std::vector<decltype(*it.first)> adj_vec;
-      for (;it.first != it.second; it.first++)
-        adj_vec.push_back(*it.first);
-
-      tbb::parallel_for((size_t)0, adj_vec.size(),
-          [&graph, &vertice, &pqueue, &dist_map](auto it)
+      for (auto it = boost::adjacent_vertices(vertice, graph);
+            it.first != it.second; it.first++)
+      {
+          auto alt = dist_map[vertice] + pythagore(graph, vertice, *it.first);
+          if (alt < dist_map[*it.first])
           {
-            auto alt = dist_map[vertice] + pythagore(graph, vertice, it);
-            if (alt < dist_map[it])
-            {
-              dist_map[it] = alt;
-              pqueue.emplace(it, alt);
-            }
-          });
-   }
-
+             dist_map[*it.first] = alt;
+             pqueue.emplace(*it.first, alt);
+          }
+       }
+    }
    return dist_map;
 }
